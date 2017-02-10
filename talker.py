2be7b07bb901466fc24fb9f0a4ead7bf3205b10c
ewbrower <ewbrower@gmail.com>
2017-02-10 17:48:39 -0500

    removed master - added to talker (not demo ready yet)

--- talker.py
+++ talker.py
@@ -1,44 +1,65 @@
-import master
 import sys
 import json
 
-class Reader():
-	"""This thing parses json"""
+class Master(object):
+	"""This is the master class that holds the connection to the MBEE server"""
+	def __init__(self, host, port):
+		self.host = host
+		self.port = port
+		print(str(sys.version_info.major) + "." + str(sys.version_info.minor))
+		self.authenticate()
 
-	def readFile(self, file):
-		print("Opening file: " + str(file))
-		js = open(file, 'r')
+	def authenticate(self):
+		print("Please enter username and password")
 
-		jsonStr = ""
-		for line in js.readlines():
-			jsonStr+=line.strip()
+	def connect(self):
+		print("connecting")
 
-		return self.readString(jsonStr)
+class Reader(Master):
+	"""This class reads information from the port"""
+	def __init__(self, host, port):
+		super(Reader, self).__init__(host, port)
 
-	def readString(self, string):
-		data = json.loads(string)
-		return data
+	def getElementById(self, id):
+		print(id)
+		# construct URL
+		# use requests to send it over
 
-class Parser(object):
-	"""This thing parses MBEE Elements"""
+	def saveElementById(self, id, filename):
+		print(id)
+		print(filename)
 
-	def __init__(self, arg):
-		self.element = arg
+	# def readFile(self, file):
+	# 	print(file)
+	# 	js = open(file, 'r')
 
-	def getSlotValue(self):
-		return self.element["specialization"]["value"][0]["double"]
-		
+	# 	jsonStr = ""
+	# 	for line in js.readlines():
+	# 		jsonStr+=line.strip()
+
+	# 	return self.readString(jsonStr)
+
+	# def readString(self, jsonStr):
+	# 	data = json.loads(jsonStr)
+	# 	return data
+
+class Writer(Master):
+	"""This class sends information over to the server"""
+	def __init__(self, host, port):
+		super(Writer, self).__init__(host, port)
+
+	def postElementFromFile(self, filename):
+		print(filename)
 
 if __name__ == '__main__':
-	m = master.Master("host","port")
+	reader = Reader("host", "port")
+	reader.getElementById("1800")
+	reader.saveElementById("1800", "sample.json")
 
-	print("Running on Python " + str(sys.version_info.major) 
-		+ "." + str(sys.version_info.minor))
-	reader = Reader()
-	jsonData = reader.readFile("sample.json")
-	element = jsonData["elements"][0]
+	writer = Writer("host", "port")
+	writer.postElementFromFile("sample.json")
 
-	parser = Parser(element)
-	slotValue = parser.getSlotValue()
-	print(slotValue)
+
+
+
 
