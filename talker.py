014298a346fb73ace9b9e806a3a554b88323037e
ewbrower <ewbrower@gmail.com>
2017-02-12 22:27:07 -0500

    it finally works - need to change how we parse the ‘elements’ list in the json

--- talker.py
+++ talker.py
@@ -1,75 +1,61 @@
 import requests
-from requests.auth import AuthBase
 import sys
 import json
 from getpass import getpass
 
-class MBEEAuth(AuthBase):
-	"""This is the authentication class for MBEE"""
-	def __init__(self):
-		super(MBEEAuth, self).__init__()
-		self.authenticate()
-
-	def __call__(self, r):
-		r.auth = self.auth
-		return r
-
-	def authenticate(self):
-		print("Please enter your username and password")
-		self.username = input("Username: ")
-		password = getpass("Password: ")
-		self.auth = (self.username, password)
-
-	def getUsername(self):
-		return self.username
-
 class MBEESession(requests.Session):
-	"""This is a requests Session object built for connecting to MBEE"""
+	'''This is a requests Session object built for connecting to MBEE'''
 
 	def __init__(self, host, port = None):
 		super(MBEESession, self).__init__()
-		print("Running with Python " + str(sys.version_info.major) 
-			+ "." + str(sys.version_info.minor) + "\n")
+		print('Running with Python ' + str(sys.version_info.major) 
+			+ '.' + str(sys.version_info.minor) + '\n')
 		# establish the baseURL for this (don't need port all the time, always host)
-		self.baseURL = "http://" + host + (":" + port if port else "") + "/alfresco/service"
-		# store custom authentication for use in the get and post functions
-		self.auth = MBEEAuth()
-
-	def connect(self):
-		print("Connecting " + self.getUser() + " to " + self.getBaseURL())
-
-	def getAuth(self):
-		return self.auth
+		self.baseURL = 'http://' + host + (':' + port if port else '') + '/alfresco/service'
+		# establish authentication for the session
+		print('Please enter your username and password')
+		username = input('Username: ')
+		self.auth = requests.auth.HTTPBasicAuth(username, getpass('Password: '))
+		self.headers.update({'content-type' : 'application/json'})
 
 	def getBaseURL(self):
 		return self.baseURL
 
-	def get(url, **kwargs):
-		print("getting " + url)
-
 class Talker(MBEESession):
-	"""This is the class that combines read and write to database methods"""
+	'''This is the class that combines read and write to database methods'''
 	def __init__(self, host, port= None):
 		super(Talker, self).__init__(host, port)
 
 	def help(self):
-		print("Here are the functions available:\ngetElementById(site, id)"
-			+ "\nsaveElementById(site, id, file)\npostElementFromFile(id, file)")
+		print('Here are the functions available:\ngetElementById(site, id)'
+			+ '\nsaveElementById(site, id, file)\npostElementFromFile(id, file)')
 
 	def getElementById(self, site, idd):
-		# construct URL
-		# use requests to send it over
-		url = self.getBaseURL() + "/workspaces/master/sites/" + site + "/elements/" + idd
-		resp = self.get(url, auth = self.getAuth())
-		response = "{this}"
-		return response
+		# construct URL use requests to send it over
+		url = self.getBaseURL() + '/workspaces/master/sites/' + site + '/elements/' + idd
+		response = self.get(url)
+		return response.json()
 
 	def saveElementById(self, site, idd, filename):
-		url = self.getElementById(site, idd)
-		return url
+		data = self.getElementById(site, idd)
+		with open(filename, 'w') as outFile:
+			json.dump(data, outFile, indent=4)
 
-	def postElementFromFile(self, idd, filename):
-		print(filename)
+	def postElementsFromFile(self, filename, idd = None):
+		# this function isn't going to work for more than one result
+		with open(filename, 'r') as inputFile:
+			data = json.load(inputFile)
+		site = self.findElementSite(data['elements'][0])
+		return self.postElementById(site, data)
+
+	def postElementById(self, site, payload):
+		url = self.getBaseURL() + '/workspaces/master/sites/' + site + '/elements'
+		response = self.post(url, data=json.dumps(payload))
+		return response
+
+	def findElementSite(self, jData):
+		site = jData['siteCharacterizationId']
+		return site
 
 
 
